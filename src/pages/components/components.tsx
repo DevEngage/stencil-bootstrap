65ba165c5aeb6a2ec81b5c4cc9f842555b135046
James Brinkerhoff <th3brink@gmail.com>
2018-03-08 00:59:12 -0700

    showed different way to select modal

--- src/pages/components/components.tsx
+++ src/pages/components/components.tsx
@@ -13,8 +13,20 @@ export class StbPageComponents {
     modal.show();
   }
 
+  open2ndModal() {
+    const modal: any = document.querySelector('#second-modal');
+    console.log(modal);
+    modal.show();
+  }
+
   closeModal() {
     const modal = document.querySelector('stb-modal');
+    console.log(modal);
+    modal.hide();
+  }
+
+  close2ndModal() {
+    const modal: any = document.querySelector('#second-modal');
     console.log(modal);
     modal.hide();
   }
@@ -27,6 +39,9 @@ export class StbPageComponents {
         <h2>Modals</h2>
 
         <button class="btn" onClick={this.openModal}>Open Modal</button>
+        <br/>
+        <br/>
+        <button class="btn" onClick={this.open2ndModal}>Open 2nd Modal</button>
 
         <stb-modal>
           <div slot="modal-dialog" class="modal-content">
@@ -46,6 +61,23 @@ export class StbPageComponents {
           </div>
         </stb-modal>
 
+        <stb-modal id="second-modal">
+          <div slot="modal-dialog" class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Modal title 2</h5>
+              <button type="button" class="close" onClick={this.close2ndModal}>
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              ...
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" onClick={this.close2ndModal}>Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+          </div>
+        </stb-modal>
       </div>
     );
   }
